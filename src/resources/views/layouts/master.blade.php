6e28b30893cc3ccad155afbdea72b96600275e1c
patrickRobotics <pirungu9@gmail.com>
2020-04-07 00:21:16 +0300

    handle requests and responses

--- src/resources/views/layouts/master.blade.php
+++ src/resources/views/layouts/master.blade.php
@@ -9,8 +9,9 @@
             href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
             integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
             crossorigin="anonymous"> 
+        <link href='http://fonts.googleapis.com/css?family=Arizonia' rel='stylesheet' type='text/css'>
 
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" 
+        <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" 
             integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" 
             crossorigin="anonymous">
         </script>
@@ -22,7 +23,8 @@
             src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" 
             integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" 
             crossorigin="anonymous">
-        </script>
+        </script> -->
+        <link rel="stylesheet" href="{{ URL::to('css/styles.css') }}">
         <title>Laravel</title>
     </head>
     <body>
--- src/resources/views/partials/header.blade.php
+++ src/resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg">
-  <a class="navbar-brand" href="#">Laravel Guide</a>
+  <a class="navbar-brand" href="{{ route('blog.index') }}">Laravel</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -7,14 +7,22 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="{{ route('blog.index') }}">Blog</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link" href="{{ route('other.about') }}">About</a>
       </li>
     </ul>
   </div>
 </nav>
+<!-- <nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="{{ route('blog.index') }}" class="navbar-brand">Laravel Guide</a>
+            <ul class="nav navbar-nav">
+                <li><a href="{{ route('blog.index') }}">Blog</a></li>
+                <li><a href="{{ route('other.about') }}">About</a></li>
+            </ul>
+        </div>
+    </div>
+</nav> -->
